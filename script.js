cda39e0906196e22e688be8ea2d8b981043d39c6
glebICT <147143396+glebICT@users.noreply.github.com>
2025-01-23 19:06:37 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function check(event){
-  
+    alert("From submitted"); // Alert the user that the form was submitted
+    event.preventDefault(); // Prevent the form from submitting
+    
+    // Access the input value
+    const inputValue = event.target.userAnswer.value; // Assuming the input has the id 'userAnswer'
+    console.log(inputValue); // Log the input value
+    // You can add further processing of inputValue here
 }
