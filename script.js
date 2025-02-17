1551881fd2a7cadb5137169a442f4c9c92beec51
glebICT <gleb.svechnikov@proton.me>
2025-02-17 15:32:04 +0400

    update project

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function check(event){
-   
+
     event.preventDefault(); // Prevent the form from submitting
-    
     // Access the input value
     const inputValue = event.target.userAnswer.value; // Assuming the input has the id 'userAnswer'
      if (inputValue == 'lego') {
         alert("Answer correct");
+        const video = document.getElementById('videoPlayer')
+        video.children[0].src = 'cube.mp4'
+        video.load()
         return false;
     }
    else{
