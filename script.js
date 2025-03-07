f153b38581bb9ff6a0c2f8f3ec91a0ad207d2d25
glebICT <gleb.svechnikov@proton.me>
2025-03-07 09:12:22 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,28 @@ function check(event){
 
     event.preventDefault(); // Prevent the form from submitting
     // Access the input value
+    let score = document.getElementById('score')
     const inputValue = event.target.userAnswer.value; // Assuming the input has the id 'userAnswer'
-     if (inputValue == 'lego') {
+    const video = document.getElementById('videoPlayer')
+    if (inputValue == 'lego') {
         alert("Answer correct");
-        const video = document.getElementById('videoPlayer')
+        score.value = Number(score.value) + 1
+      
         video.children[0].src = 'cube.mp4'
         video.load()
         return false;
     }
+    if (inputValue == 'cube') {
+      alert("Answer correct");
+      score.value = Number(score.value) + 1
+      // const video = document.getElementById('videoPlayer')
+      // video.children[0].src = 'cube.mp4'
+      // video.load()
+      return false;
+  }
    else{
+      score.value = Number(score.value) - 1
+
       alert("Answer incorrect");
         return false;
    }
