66adfe7a59d9069996d20506155c5ef903d0ecb9
glebICT <147143396+glebICT@users.noreply.github.com>
2025-03-16 20:55:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-function check(event){
 
-    event.preventDefault(); // Prevent the form from submitting
-    // Access the input value
+
+function check(event){
+    event.preventDefault();
     let score = document.getElementById('score')
-    const inputValue = event.target.userAnswer.value; // Assuming the input has the id 'userAnswer'
+    let level = document.getElementById('level') // Get level element
+    const inputValue = event.target.userAnswer.value;
     const video = document.getElementById('videoPlayer')
-    if (inputValue == 'lego') {
+    
+    if (inputValue == 'lego' && Number(level.value) == 1) {
         alert("Answer correct");
         score.value = Number(score.value) + 1
-      
+        level.value = Number(level.value) + 1  // Move to level 2
         video.children[0].src = 'cube.mp4'
         video.load()
         return false;
     }
-    if (inputValue == 'cube') {
+
+    if (inputValue == 'cube' && Number(level.value) == 2) {
+        alert("Answer correct");
+        score.value = Number(score.value) + 1
+        level.value = Number(level.value) + 1  // Move to level 3
+        video.children[0].src = 'planet.mp4'
+        video.load()
+        return false;
+    }
+    if (inputValue == 'planet' && Number(level.value) == 3) {
       alert("Answer correct");
       score.value = Number(score.value) + 1
-      // const video = document.getElementById('videoPlayer')
-      // video.children[0].src = 'cube.mp4'
-      // video.load()
+      level.value = Number(level.value) + 1  // Move to level 3
       return false;
   }
-   else{
-      score.value = Number(score.value) - 1
-
-      alert("Answer incorrect");
+    else {
+        score.value = Number(score.value) - 1
+        alert("Answer incorrect or wrong level!");
         return false;
-   }
+    }
 }
